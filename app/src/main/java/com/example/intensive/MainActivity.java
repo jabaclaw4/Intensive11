c7a3eb3f9ff8899c8c32472e5a1940a5520cf57f
anastasiia <nastya.v.krutilina@gmail.com>
2024-01-21 18:46:00 +0300

    Initial commit

---
+++ app/src/main/java/com/example/intensive/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.intensive;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.appbar.MaterialToolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
